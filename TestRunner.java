5937723a248eb93c70a1cf0ad503ceacb1e52855
virat <sriramadesigan045@gmail.com>
2021-12-25 13:15:22 +0530

    gits

---
+++ TestRunner.java
@@ -0,0 +1,13 @@
+package com.runnerPackage;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features= {"src/test/resources"},glue= {"com.stepdefinitionPackage"})
+public class TestRunner {
+
+	
+}
